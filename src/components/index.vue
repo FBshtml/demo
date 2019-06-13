2312e19cc3fc219c3f957e2b75b35a3571bea921
Fbshtml <2679450438@qq.com>
2019-06-13 11:02:55 +0800

    1

---
+++ src/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+   <div>el-tabs</div>
+    <template>
+      <el-tabs>
+        <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      </el-tabs>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+}
+</script>
+
+<style >
+.el-tabs__nav-wrap {
+  /* ??????? */
+  padding-left: 50px; 
+}
+</style>
